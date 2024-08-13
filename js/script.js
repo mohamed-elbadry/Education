f5d19de18192bd76350a8d56c81c449b8996dca8
mohamed-elbadry <mohmedelbadry25@gmail.com>
2024-08-13 14:25:57 +0300

    .educ

--- js/script.js
+++ js/script.js
@@ -166,6 +166,46 @@ bulletsSpan.forEach(span => {
      progressWrap.classList.remove('active-progress');
    }
  });
+
+// Get all the card-overlay-container elements
+const cardOverlayContainers = document.querySelectorAll('.card');
+
+// Add event listeners to each container
+cardOverlayContainers.forEach((container) => {
+  container.addEventListener('mouseenter', handleMouseEnter);
+  container.addEventListener('mouseleave', handleMouseLeave);
+});
+
+function handleMouseEnter(e) {
+  const overlay = e.currentTarget.querySelector('.overlay');
+  const rect = e.currentTarget.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  let direction;
+  if (x < rect.width / 2 && y < rect.height / 2) {
+    direction = 'top-left';
+  } else if (x >= rect.width / 2 && y < rect.height / 2) {
+    direction = 'top-right';
+  } else if (x < rect.width / 2 && y >= rect.height / 2) {
+    direction = 'bottom-left';
+  } else {
+    direction = 'bottom-right';
+  }
+
+  overlay.classList.add(`overlay--${direction}`);
+  overlay.classList.add('show');
+}
+
+function handleMouseLeave(e) {
+  const overlay = e.currentTarget.querySelector('.overlay');
+  overlay.classList.remove('show');
+  overlay.classList.forEach((cls) => {
+    if (cls.startsWith('overlay--')) {
+      overlay.classList.remove(cls);
+    }
+  });
+}
 })
 
 
@@ -239,9 +279,7 @@ $('#testimonial-cars').owlCarousel({
   loop:true,
   margin:10,
   nav:true,
-  dots:true,
-  dotsEach:1,
-   slideBy: 1,
+  dots:false,
   navText:[
     `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="#fff" d="M17.59 18L19 16.59L14.42 12L19 7.41L17.59 6l-6 6z"/><path fill="#fff" d="m11 18l1.41-1.41L7.83 12l4.58-4.59L11 6l-6 6z"/></svg>`,
     `<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="#fff" d="M6.41 6L5 7.41L9.58 12L5 16.59L6.41 18l6-6z"/><path fill="#fff" d="m13 6l-1.41 1.41L16.17 12l-4.58 4.59L13 18l6-6z"/></svg>`
